98735bcdae9fac9d21fa68dc65ae92c4e75fe9c0
zamzamnurahman <kazamnn1912@gmail.com>
2022-10-15 20:42:47 +0700

    add container

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -35,44 +36,92 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       backgroundColor: const Color(0xffF7F6FB),
       body: SafeArea(
-          child: Container(
-        height: 100,
-        padding: const EdgeInsets.all(20),
-        // decoration: BoxDecoration(color: Colors.red),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
+          child: Column(
+        children: [
+          Container(
+            height: 100,
+            padding: const EdgeInsets.all(20),
+            // decoration: BoxDecoration(color: Colors.red),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                const CircleAvatar(
-                  backgroundColor: Colors.white,
-                  radius: 30,
-                ),
-                Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Text(
-                        'Hi, Nama',
-                        style: TextStyle(
-                            fontSize: 24, fontWeight: FontWeight.bold),
+                Row(
+                  children: [
+                    const CircleAvatar(
+                      backgroundColor: Colors.blueAccent,
+                      radius: 30,
+                    ),
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text(
+                            'Hi, Nama',
+                            style: TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.bold),
+                          ),
+                          Text('Selamat Datang!'),
+                        ],
                       ),
-                      Text('Selamat Datang!'),
-                    ],
-                  ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: const [
+                    Icon(Icons.menu),
+                  ],
                 ),
               ],
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: const [
-                Icon(Icons.menu),
+          ),
+          Container(
+            height: 90,
+            width: MediaQuery.of(context).size.width,
+            margin: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width * 0.05,
+            ),
+            decoration: BoxDecoration(
+                color: const Color(0xff256D85),
+                borderRadius: BorderRadius.circular(10)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      "0",
+                      style: TextStyle(
+                        fontSize: 48,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text("Tugas", style: TextStyle(color: Colors.white)),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      "0",
+                      style: TextStyle(
+                        fontSize: 48,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text("Materi", style: TextStyle(color: Colors.white)),
+                  ],
+                )
               ],
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       )),
     );
   }
