ab2ee4f105a2c0fc852eea8afad72ca2757ec821
zamzamnurahman <kazamnn1912@gmail.com>
2022-10-25 22:49:56 +0700

    add hero section

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,5 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import '../utils/theme.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -15,43 +12,98 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: kWhiteBg,
         body: SafeArea(
-      child: navbar(),
-    ));
+            child: Container(
+          padding: const EdgeInsets.all(34),
+          child: Column(
+            children: [
+              navbar(),
+              hero(context),
+            ],
+          ),
+        )));
   }
 }
 
 Widget navbar() {
-  return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Expanded(
-                child: Row(
-                  children: [
-                    CircleAvatar(
-                      backgroundImage: AssetImage("assets/images/profile.jpg"),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Nama Lengkap",
-                            style: TextStyle(fontWeight: FontWeight.w700)),
-                        Text("Semester N/A")
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              IconButton(onPressed: () {}, icon: Icon(Icons.menu))
-            ],
-          )
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text("Nama Lengkap",
+              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 16)),
+          Text("Semester N/A")
         ],
-      ));
+      ),
+      const CircleAvatar(
+        backgroundImage: AssetImage("assets/images/profile.jpg"),
+      ),
+    ],
+  );
+}
+
+Widget hero(BuildContext context) {
+  return Container(
+    margin: const EdgeInsets.symmetric(vertical: 30),
+    height: 166,
+    width: MediaQuery.of(context).size.width,
+    decoration: BoxDecoration(
+      color: kGreenPrimary,
+      borderRadius: BorderRadius.circular(20),
+    ),
+    child: Container(
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text("ELearning ITG",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700,
+                      color: kWhiteBg,
+                    )),
+                Text("Layanan Digitalisasi Sekolah",
+                    style: TextStyle(color: kWhiteBg)),
+              ],
+            ),
+            Container(
+              height: 35,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  color: kWhiteBg, borderRadius: BorderRadius.circular(10)),
+              child: Row(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                    child: Icon(
+                      Icons.search,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  Expanded(
+                      child: TextFormField(
+                    decoration: const InputDecoration(
+                        hintText: "Silahkan Cari ...",
+                        hintStyle: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey,
+                        ),
+                        isDense: true,
+                        isCollapsed: true,
+                        border:
+                            OutlineInputBorder(borderSide: BorderSide.none)),
+                  ))
+                ],
+              ),
+            )
+          ]),
+    ),
+  );
 }
---
+++ lib/utils/theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const kGreenPrimary = Color(0xff256D85);
+const kGreen = Color(0xff06283D);
+const kWhiteBg = Color(0xffF7F6FB);
